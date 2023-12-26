10cd26760ed968df32b71ee44102c610442d9297
Kwaay <benoit.dum74@gmail.com>
2023-12-26 23:54:01 +0100

    ✨ Feature | UserEdit Page

---
+++ src/pages/UserEdit/index.jsx
@@ -0,0 +1,71 @@
+/* eslint-disable dot-notation */
+import { useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
+import Header from '../../components/Header';
+import Footer from '../../components/Footer';
+import Transaction from '../../components/Transaction';
+import store from '../../store';
+import transactionsData from '../../data/transactions';
+import styles from './UserEdit.module.css';
+import { addUser } from '../../store/UserSlice';
+
+export default function UserEdit() {
+  const [user, setUser] = useState(store.getState().user);
+  const dispatch = useDispatch();
+
+  const listTransactions = transactionsData.map((transaction) => (
+    <Transaction {...transaction} key={transaction.name} />
+  ));
+
+  useEffect(() => {
+    const token = store.getState().auth.auth;
+    fetch('http://localhost:3001/api/v1/user/profile', {
+      method: 'POST',
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+      .then((res) => {
+        if (res.ok) {
+          return res.json();
+        }
+        throw new Error('Unable to get user data');
+      })
+      .then(({ body }) => {
+        dispatch(addUser(body));
+        setUser(body);
+      });
+  }, []);
+  return (
+    <div className="App">
+      <Header auth={store.getState().auth.auth} />
+      <main className={styles['container']}>
+        <div className={styles['user-modify']}>
+          <h1>Welcome back</h1>
+          <form className={styles['form-container']}>
+            <div className={styles['form-group']}>
+              <input
+                type="text"
+                id="firstname"
+                placeholder={user.firstName}
+                className={styles['form-firstname']}
+              />
+              <input
+                type="text"
+                id="lastname"
+                placeholder={user.lastName}
+                className={styles['form-lastname']}
+              />
+            </div>
+            <div className={styles['form-group']}>
+              <button type="submit">Save</button>
+              <button type="submit">Cancel</button>
+            </div>
+          </form>
+        </div>
+        <div className={styles['transactions']}>{listTransactions}</div>
+      </main>
+      <Footer />
+    </div>
+  );
+}
